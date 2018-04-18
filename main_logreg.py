9902b2bd2c5a549c2870aa0b68fb2b1ebdbf45b6
Manfred Böck <manfred.boeck@student.tugraz.at>
2018-04-18 16:15:17 +0200

    added final plots

--- main_logreg.py
+++ main_logreg.py
@@ -21,9 +21,9 @@ This is the main file that loads the data, compute the solution and plot the res
 
 def main():
     # Set parameters
-    degree = 2
-    eta = 15
-    max_iter = 200
+    degree = 1
+    eta = 1
+    max_iter = 10
 
     # Load data and expand with polynomial features
     f = open('data_logreg.json', 'r')
@@ -46,9 +46,9 @@ def main():
     theta0 = np.zeros(n)
 
     #### VARIANT 1: Optimize with gradient descent
-    theta_opt, E_list = gd.gradient_descent(f, df, theta0, eta, max_iter)
+    # theta_opt, E_list = gd.gradient_descent(f, df, theta0, eta, max_iter)
 
-    #### VARIANT 2: Optimize with gradient descent
+    ### VARIANT 2: Optimize with gradient descent
     # theta_opt, E_list, lr_list = gd.adaptative_gradient_descent(f, df, theta0, eta, max_iter)
     # plt.plot(lr_list)
     # plt.xlabel('Iterations')
@@ -56,9 +56,9 @@ def main():
     # print('Adaptative gradient, final learning rate: {:.3g}'.format(lr_list[-1]))
 
     #### VARIANT 3: Optimize with gradient descent
-    # res = minimize(f, x0=theta0, jac=df, options={'disp': True ,'maxiter': max_iter})
-    # theta_opt = res.x.reshape((n, 1))
-    # E_list = []
+    res = minimize(f, x0=theta0, jac=df, options={'disp': True ,'maxiter': max_iter})
+    theta_opt = res.x.reshape((n, 1))
+    E_list = []
 
     logreg_toolbox.plot_logreg(data, degree, theta_opt, E_list)
     plt.show()
