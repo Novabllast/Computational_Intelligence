12dfc6dcaf3c0c3db99e563b97e84e3436029ae1
Manfred Böck <manfred.boeck@student.tugraz.at>
2018-04-26 14:25:14 +0200

    ex_2_2 almost completed

--- Assignment2/nn_classification.py
+++ Assignment2/nn_classification.py
@@ -1,4 +1,4 @@
-from sklearn.metrics import confusion_matrix, mean_squared_error
+from sklearn.metrics import confusion_matrix, mean_squared_error, accuracy_score
 
 from sklearn.neural_network.multilayer_perceptron import MLPClassifier
 from nn_classification_plot import *
@@ -47,7 +47,7 @@ def ex_2_1(input2, target2):
 
     y_pred = mlp_classifier.predict(input2)
     matrix = confusion_matrix(pose, y_pred)
-    print("The Confusion Matrix we obtained: ", matrix)
+    print("The Confusion Matrix we obtained: \n" + str(matrix))
 
     plot_hidden_layer_weights(hidden_layer_weights)
 
@@ -69,10 +69,38 @@ def ex_2_2(input1, target1, input2, target2):
 
     train_acc = []
     test_acc = []
-    misclassified_images = []
+    training_person = target1[:, 0]
+    test_person = target2[:, 0]
+
     for i in range(0, 10):
         mlp_classifier.random_state = i
-        mlp_classifier.fit(input1, target1)
+        mlp_classifier.fit(input1, training_person)
+        predict_test = mlp_classifier.predict(input2)
+        predict_training = mlp_classifier.predict(input1)
+        # test_acc.append(mlp_classifier.score(test_person, predict_test))      # TODO throws exception
+        # train_acc.append(mlp_classifier.score(training_person, predict_training))# TODO throws exception
+        test_acc.append(accuracy_score(test_person, predict_test))
+        train_acc.append(accuracy_score(training_person, predict_training))
+
+    best_network = max(test_acc)
+    best_network_index = test_acc.index(best_network)
+
+    mlp_classifier.random_state = best_network_index
+    mlp_classifier.fit(input1, training_person)
+
+    y_pred = mlp_classifier.predict(input2)
+    matrix = confusion_matrix(test_person, y_pred)
+    print("The Confusion Matrix we obtained: \n" + str(matrix))
 
     plot_histogram_of_acc(train_acc, test_acc)
-    plot_random_images(misclassified_images)
+
+    annas_favorit_number = 177
+    strugers_favorit_number = 42
+    marcos_favorit_numer = 490357
+    manfreds_favorit_number_aka_best_number = 7
+    best_numbers_ever = [annas_favorit_number, strugers_favorit_number, marcos_favorit_numer,
+                         manfreds_favorit_number_aka_best_number]
+
+    for i in best_numbers_ever:
+        misclassified_images = input2[i, :]
+        # plot_random_images(misclassified_images) # TODO throws exception
