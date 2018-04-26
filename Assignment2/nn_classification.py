7e42d6f42deef94ed1875f06225f3f3241b585ca
Patrick Struger <pstruger@student.tugraz.at>
2018-04-26 22:23:28 +0200

    Some minor changes in classification. Working on face recognition.

--- Assignment2/nn_classification.py
+++ Assignment2/nn_classification.py
@@ -1,7 +1,7 @@
 from sklearn.metrics import confusion_matrix, mean_squared_error, accuracy_score
 
 from sklearn.neural_network.multilayer_perceptron import MLPClassifier
-from nn_classification_plot import *
+from Assignment2.nn_classification_plot import *
 import numpy as np
 from sklearn.metrics import confusion_matrix
 
@@ -34,20 +34,23 @@ def ex_2_1(input2, target2):
     """
     ## TODO
 
-    iteration = 200
     hidden_units = 6
-    mlp_classifier = MLPClassifier(activation=ACTIVATION, solver='adam', hidden_layer_sizes=(hidden_units,),
-                                   max_iter=iteration)
+    nn = MLPClassifier(activation=ACTIVATION, solver='adam', alpha=0.0, hidden_layer_sizes=(hidden_units,),
+                                   max_iter=200)
     pose = target2[:, 1]
-    mlp_classifier.fit(input2, pose)
-    coefs_ = mlp_classifier.coefs_
-    hidden_layer_weights = coefs_[0]  # using index zero because using index 1 => exception
+    nn.fit(input2, pose)
 
-    y_pred = mlp_classifier.predict(input2)
+    # using index 0 because of just one hidden layer
+    hidden_layer_weights = nn.coefs_[0]
+
+    y_pred = nn.predict(input2)
     matrix = confusion_matrix(pose, y_pred)
+
     print("The Confusion Matrix we obtained: \n" + str(matrix))
 
-    plot_hidden_layer_weights(hidden_layer_weights) # TODO
+    plot_hidden_layer_weights(hidden_layer_weights)
+
+    pass
 
 
 def ex_2_2(input1, target1, input2, target2):
@@ -60,45 +63,57 @@ def ex_2_2(input1, target1, input2, target2):
     :return:
     """
     ## TODO
-    iteration = 1000
     hidden_units = 20
-    mlp_classifier = MLPClassifier(activation=ACTIVATION, solver="adam", hidden_layer_sizes=(hidden_units,),
-                                   max_iter=iteration)
 
-    train_acc = []
-    test_acc = []
-    training_person = target1[:, 0]
-    test_person = target2[:, 0]
+    test_face = target2[:, 0]
+    train_face = target1[:, 0]
 
+    test_accuracy = np.zeros(10)
+    train_accuracy = np.zeros(10)
     for i in range(0, 10):
-        mlp_classifier.random_state = i
-        mlp_classifier.fit(input1, training_person)
-        predict_test = mlp_classifier.predict(input2)
-        predict_training = mlp_classifier.predict(input1)
-        # test_acc.append(mlp_classifier.score(test_person, predict_test))      # TODO throws exception
-        # train_acc.append(mlp_classifier.score(training_person, predict_training))# TODO throws exception
-        test_acc.append(accuracy_score(test_person, predict_test))
-        train_acc.append(accuracy_score(training_person, predict_training))
+        nn = MLPClassifier(activation=ACTIVATION, solver="adam", alpha=0.0, hidden_layer_sizes=(hidden_units,),
+                                   max_iter=1000, random_state=i)
 
-    best_network = max(test_acc)
-    best_network_index = test_acc.index(best_network)
+        nn.fit(input1, train_face)
+        test_accuracy[i] = nn.score(test_face, nn.predict(input2))
+        train_accuracy[i] = nn.score(train_face, nn.predict(input1))
 
-    mlp_classifier.random_state = best_network_index
-    mlp_classifier.fit(input1, training_person)
+    #plot_histogram_of_acc(train_accuracy, test_accuracy)
 
-    y_pred = mlp_classifier.predict(input2)
-    matrix = confusion_matrix(test_person, y_pred)
-    print("The Confusion Matrix we obtained: \n" + str(matrix))
-
-    plot_histogram_of_acc(train_acc, test_acc)
-
-    annas_favorit_number = 177
-    strugers_favorit_number = 42
-    marcos_favorit_numer = 490
-    manfreds_favorit_number_aka_best_number = 7
-    best_numbers_ever = [annas_favorit_number, strugers_favorit_number, marcos_favorit_numer,
-                         manfreds_favorit_number_aka_best_number]
-
-    for i in best_numbers_ever:
-        misclassified_images = input2[i, :]
-        plot_random_images(misclassified_images) # TODO throws exception
+    # train_acc = []
+    # test_acc = []
+    # training_person = target1[:, 0]
+    # test_person = target2[:, 0]
+    #
+    # for i in range(0, 10):
+    #     mlp_classifier.random_state = i
+    #     mlp_classifier.fit(input1, training_person)
+    #     predict_test = mlp_classifier.predict(input2)
+    #     predict_training = mlp_classifier.predict(input1)
+    #     #test_acc.append(mlp_classifier.score(test_person, predict_test))      # TODO throws exception
+    #     #train_acc.append(mlp_classifier.score(training_person, predict_training))# TODO throws exception
+    #     test_acc.append(accuracy_score(test_person, predict_test))
+    #     train_acc.append(accuracy_score(training_person, predict_training))
+    #
+    # best_network = max(test_acc)
+    # best_network_index = test_acc.index(best_network)
+    #
+    # mlp_classifier.random_state = best_network_index
+    # mlp_classifier.fit(input1, training_person)
+    #
+    # y_pred = mlp_classifier.predict(input2)
+    # matrix = confusion_matrix(test_person, y_pred)
+    # print("The Confusion Matrix we obtained: \n" + str(matrix))
+    #
+    # plot_histogram_of_acc(train_acc, test_acc)
+    #
+    # annas_favorit_number = 177
+    # strugers_favorit_number = 42
+    # marcos_favorit_numer = 490
+    # manfreds_favorit_number_aka_best_number = 7
+    # best_numbers_ever = [annas_favorit_number, strugers_favorit_number, marcos_favorit_numer,
+    #                      manfreds_favorit_number_aka_best_number]
+    #
+    # for i in best_numbers_ever:
+    #     misclassified_images = input2[i, :]
+    #     plot_random_images(misclassified_images) # TODO throws exception
